04d51b8d9db1ffa3bd9e181e1221fe80e91f5798
Philippe Antoine <contact@catenacyber.fr>
2019-02-06 15:22:14 +0100

    Threads for linux
    And cmakelists for compiling lzma as a library
    And a first fuzz target

--- C/Threads.c
+++ C/Threads.c
@@ -4,11 +4,19 @@
 #include "Precomp.h"
 
 #ifndef UNDER_CE
+#ifdef _WIN32
 #include <process.h>
+#endif
+#endif
+
+#ifndef _WIN32
+#include <strings.h>
+#include <errno.h>
 #endif
 
 #include "Threads.h"
 
+#ifdef _WIN32
 static WRes GetError()
 {
   DWORD res = GetLastError();
@@ -30,35 +38,159 @@ WRes HandlePtr_Close(HANDLE *p)
 }
 
 WRes Handle_WaitObject(HANDLE h) { return (WRes)WaitForSingleObject(h, INFINITE); }
+#endif
 
 WRes Thread_Create(CThread *p, THREAD_FUNC_TYPE func, LPVOID param)
 {
   /* Windows Me/98/95: threadId parameter may not be NULL in _beginthreadex/CreateThread functions */
-  
+  int ret;
+
+  p->_created = 0;
   #ifdef UNDER_CE
   
   DWORD threadId;
-  *p = CreateThread(0, 0, func, param, 0, &threadId);
-
-  #else
+  p->hnd = CreateThread(0, 0, func, param, 0, &threadId);
+  ret = HandleToWRes(p->hnd);
+  #elif defined(_WIN32)
 
   unsigned threadId;
-  *p = (HANDLE)_beginthreadex(NULL, 0, func, param, 0, &threadId);
-   
+  p->hnd = (HANDLE)_beginthreadex(NULL, 0, func, param, 0, &threadId);
+  ret = HandleToWRes(p->hnd);
+
+  #else
+    pthread_attr_t attr;
+    ret = pthread_attr_init(&attr);
+    if (ret) return ret;
+
+    ret = pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_JOINABLE);
+    if (ret) return ret;
+
+    ret = pthread_create(&p->tid, &attr, (void * (*)(void *))func, param);
+    //not checking return
+    pthread_attr_destroy(&attr);
+
   #endif
+  if (ret == 0) {
+    p->_created = 1;
+  }
 
   /* maybe we must use errno here, but probably GetLastError() is also OK. */
-  return HandleToWRes(*p);
+  return ret;
+}
+
+WRes Thread_Close(CThread *p) {
+#ifdef _WIN32
+    return HandlePtr_Close(&p->hnd);
+#else
+    p->_created = 0;
+    return SZ_OK;
+#endif
+}
+
+WRes Thread_Wait(CThread *p) {
+#ifdef _WIN32
+    return Handle_WaitObject(p->hnd);
+#else
+    int ret;
+
+    if (p->_created == 0) return EINVAL;
+
+    void *status;
+    ret = pthread_join(p->tid, &status);
+    p->_created = 0;
+    return ret;
+#endif
 }
 
 static WRes Event_Create(CEvent *p, BOOL manualReset, int signaled)
 {
-  *p = CreateEvent(NULL, manualReset, (signaled ? TRUE : FALSE), NULL);
-  return HandleToWRes(*p);
+    int ret;
+
+    p->_created = 0;
+#ifdef _WIN32
+  p->hnd = CreateEvent(NULL, manualReset, (signaled ? TRUE : FALSE), NULL);
+  ret = HandleToWRes(*p);
+#else
+    pthread_mutexattr_t mutexattr;
+
+    memset(&mutexattr,0,sizeof(mutexattr));
+    ret = pthread_mutexattr_init(&mutexattr);
+    if (ret) return ret;
+
+    ret = pthread_mutexattr_settype(&mutexattr,PTHREAD_MUTEX_ERRORCHECK);
+    if (ret) return ret;
+
+    ret = pthread_mutex_init(&p->_mutex,&mutexattr);
+    if (ret) return ret;
+
+    ret = pthread_cond_init(&p->_cond,0);
+
+#endif
+    if (ret) return ret;
+
+    p->_manual_reset = manualReset;
+    p->_state        = (signaled ? TRUE : FALSE);
+    p->_created = 1;
+    return ret;
 }
 
-WRes Event_Set(CEvent *p) { return BOOLToWRes(SetEvent(*p)); }
-WRes Event_Reset(CEvent *p) { return BOOLToWRes(ResetEvent(*p)); }
+WRes Event_Close(CEvent *p) {
+#ifdef _WIN32
+    return HandlePtr_Close(&p->hnd);
+#else
+    if (p->_created)
+    {
+        p->_created = 0;
+        pthread_mutex_destroy(&p->_mutex);
+        pthread_cond_destroy(&p->_cond);
+    }
+    return 0;
+#endif
+}
+
+WRes Event_Wait(CEvent *p) {
+#ifdef _WIN32
+    return Handle_WaitObject(p->hnd);
+#else
+    pthread_mutex_lock(&p->_mutex);
+    while (p->_state == FALSE)
+    {
+        pthread_cond_wait(&p->_cond, &p->_mutex);
+    }
+    if (p->_manual_reset == FALSE)
+    {
+        p->_state = FALSE;
+    }
+    pthread_mutex_unlock(&p->_mutex);
+    return 0;
+#endif
+}
+
+WRes Event_Set(CEvent *p) {
+#ifdef _WIN32
+return BOOLToWRes(SetEvent(p->hnd));
+#else
+    int ret = pthread_mutex_lock(&p->_mutex);
+    if (ret) return ret;
+
+    p->_state = TRUE;
+    ret = pthread_cond_broadcast(&p->_cond);
+    pthread_mutex_unlock(&p->_mutex);
+    return ret;
+#endif
+}
+
+WRes Event_Reset(CEvent *p) {
+#ifdef _WIN32
+return BOOLToWRes(ResetEvent(p->hnd));
+#else
+    int ret = pthread_mutex_lock(&p->_mutex);
+    if (ret) return ret;
+    p->_state = FALSE;
+    ret = pthread_mutex_unlock(&p->_mutex);
+    return ret;
+#endif
+}
 
 WRes ManualResetEvent_Create(CManualResetEvent *p, int signaled) { return Event_Create(p, TRUE, signaled); }
 WRes AutoResetEvent_Create(CAutoResetEvent *p, int signaled) { return Event_Create(p, FALSE, signaled); }
@@ -68,28 +200,134 @@ WRes AutoResetEvent_CreateNotSignaled(CAutoResetEvent *p) { return AutoResetEven
 
 WRes Semaphore_Create(CSemaphore *p, UInt32 initCount, UInt32 maxCount)
 {
-  *p = CreateSemaphore(NULL, (LONG)initCount, (LONG)maxCount, NULL);
-  return HandleToWRes(*p);
+    int ret;
+
+    p->_created = 0;
+#ifdef _WIN32
+  p->hnd = CreateSemaphore(NULL, (LONG)initCount, (LONG)maxCount, NULL);
+  ret = HandleToWRes(p->hnd);
+#else
+    pthread_mutexattr_t mutexattr;
+    memset(&mutexattr,0,sizeof(mutexattr));
+    ret = pthread_mutexattr_init(&mutexattr);
+    if (ret) return ret;
+    ret = pthread_mutexattr_settype(&mutexattr,PTHREAD_MUTEX_ERRORCHECK);
+    if (ret) return ret;
+    ret = pthread_mutex_init(&p->_mutex,&mutexattr);
+    if (ret) return ret;
+    ret = pthread_cond_init(&p->_cond,0);
+#endif
+    if (ret) return ret;
+
+    p->_count    = initCount;
+    p->_maxCount = maxCount;
+    p->_created = 1;
+    return ret;
 }
 
-static WRes Semaphore_Release(CSemaphore *p, LONG releaseCount, LONG *previousCount)
-  { return BOOLToWRes(ReleaseSemaphore(*p, releaseCount, previousCount)); }
+WRes Semaphore_Close(CSemaphore *p) {
+#ifdef _WIN32
+    return HandlePtr_Close(&p->hnd);
+#else
+    if (p->_created)
+    {
+        p->_created = 0;
+        pthread_mutex_destroy(&p->_mutex);
+        pthread_cond_destroy(&p->_cond);
+    }
+    return 0;
+#endif
+}
+
+WRes Semaphore_Wait(CSemaphore *p) {
+#ifdef _WIN32
+    return Handle_WaitObject(p->hnd);
+#else
+    pthread_mutex_lock(&p->_mutex);
+    while (p->_count < 1)
+    {
+        pthread_cond_wait(&p->_cond, &p->_mutex);
+    }
+    p->_count--;
+    pthread_mutex_unlock(&p->_mutex);
+    return 0;
+#endif
+}
+
+static WRes Semaphore_Release(CSemaphore *p, UInt32 releaseCount, UInt32 *previousCount)
+  {
+#ifdef _WIN32
+return BOOLToWRes(ReleaseSemaphore(*p, (LONG)releaseCount, (LONG)previousCount));
+#else
+    int ret;
+    if (releaseCount < 1) return EINVAL;
+
+    ret = pthread_mutex_lock(&p->_mutex);
+    if (ret) return ret;
+    UInt32 newCount = p->_count + releaseCount;
+    if (newCount > p->_maxCount) {
+        pthread_mutex_unlock(&p->_mutex);
+        return EINVAL;
+    }
+    p->_count = newCount;
+    ret = pthread_cond_broadcast(&p->_cond);
+    pthread_mutex_unlock(&p->_mutex);
+    return ret;
+#endif
+  }
 WRes Semaphore_ReleaseN(CSemaphore *p, UInt32 num)
-  { return Semaphore_Release(p, (LONG)num, NULL); }
+  { return Semaphore_Release(p, num, NULL); }
 WRes Semaphore_Release1(CSemaphore *p) { return Semaphore_ReleaseN(p, 1); }
 
 WRes CriticalSection_Init(CCriticalSection *p)
 {
+#ifdef _WIN32
   /* InitializeCriticalSection can raise only STATUS_NO_MEMORY exception */
   #ifdef _MSC_VER
   __try
   #endif
   {
-    InitializeCriticalSection(p);
+    InitializeCriticalSection(&p->cs);
     /* InitializeCriticalSectionAndSpinCount(p, 0); */
   }
   #ifdef _MSC_VER
   __except (EXCEPTION_EXECUTE_HANDLER) { return 1; }
   #endif
   return 0;
+#else
+    int ret;
+    pthread_mutexattr_t mutexattr;
+    if (!p) return EINTR;
+    memset(&mutexattr,0,sizeof(mutexattr));
+    ret = pthread_mutexattr_init(&mutexattr);
+    if (ret) return ret;
+    ret = pthread_mutexattr_settype(&mutexattr,PTHREAD_MUTEX_ERRORCHECK);
+    if (ret) return ret;
+    ret = pthread_mutex_init(&p->_mutex,&mutexattr);
+    return ret;
+#endif
+}
+
+void CriticalSection_Delete(CCriticalSection *p) {
+#ifdef _WIN32
+    DeleteCriticalSection(&p->cs);
+#else
+    pthread_mutex_destroy(&p->_mutex);
+#endif
+}
+
+void CriticalSection_Enter(CCriticalSection *p) {
+#ifdef _WIN32
+    EnterCriticalSection(&p->cs);
+#else
+    pthread_mutex_lock(&p->_mutex);
+#endif
+}
+
+void CriticalSection_Leave(CCriticalSection *p) {
+#ifdef _WIN32
+    LeaveCriticalSection(&p->cs);
+#else
+    pthread_mutex_unlock(&p->_mutex);
+#endif
 }
--- C/Threads.h
+++ C/Threads.h
@@ -6,20 +6,33 @@
 
 #ifdef _WIN32
 #include <windows.h>
+#else
+#define LPVOID void *
+#define BOOL int
+#define TRUE 1
+#define FALSE 0
+#include <pthread.h>
 #endif
 
 #include "7zTypes.h"
 
 EXTERN_C_BEGIN
 
-WRes HandlePtr_Close(HANDLE *h);
-WRes Handle_WaitObject(HANDLE h);
+typedef struct _CThread
+{
+#ifdef _WIN32
+    HANDLE hnd;
+#else
+    pthread_t tid;
+#endif
+    int _created;
+} CThread;
 
-typedef HANDLE CThread;
-#define Thread_Construct(p) *(p) = NULL
-#define Thread_WasCreated(p) (*(p) != NULL)
-#define Thread_Close(p) HandlePtr_Close(p)
-#define Thread_Wait(p) Handle_WaitObject(*(p))
+
+#define Thread_Construct(p) (p)->_created = 0
+#define Thread_WasCreated(p) ((p)->_created != 0)
+WRes Thread_Close(CThread *p);
+WRes Thread_Wait(CThread *p);
 
 typedef
 #ifdef UNDER_CE
@@ -34,13 +47,24 @@ typedef
 typedef THREAD_FUNC_RET_TYPE (THREAD_FUNC_CALL_TYPE * THREAD_FUNC_TYPE)(void *);
 WRes Thread_Create(CThread *p, THREAD_FUNC_TYPE func, LPVOID param);
 
-typedef HANDLE CEvent;
+typedef struct _CEvent
+{
+    int _created;
+    int _manual_reset;
+    int _state;
+#ifdef _WIN32
+    HANDLE hnd;
+#else
+    pthread_mutex_t _mutex;
+    pthread_cond_t  _cond;
+#endif
+} CEvent;
 typedef CEvent CAutoResetEvent;
 typedef CEvent CManualResetEvent;
-#define Event_Construct(p) *(p) = NULL
-#define Event_IsCreated(p) (*(p) != NULL)
-#define Event_Close(p) HandlePtr_Close(p)
-#define Event_Wait(p) Handle_WaitObject(*(p))
+#define Event_Construct(event) (event)->_created = 0
+#define Event_IsCreated(event) ((event)->_created)
+WRes Event_Wait(CEvent *event);
+WRes Event_Close(CEvent *event);
 WRes Event_Set(CEvent *p);
 WRes Event_Reset(CEvent *p);
 WRes ManualResetEvent_Create(CManualResetEvent *p, int signaled);
@@ -48,20 +72,38 @@ WRes ManualResetEvent_CreateNotSignaled(CManualResetEvent *p);
 WRes AutoResetEvent_Create(CAutoResetEvent *p, int signaled);
 WRes AutoResetEvent_CreateNotSignaled(CAutoResetEvent *p);
 
-typedef HANDLE CSemaphore;
-#define Semaphore_Construct(p) *(p) = NULL
+typedef struct _CSemaphore
+{
+    int _created;
+    UInt32 _count;
+    UInt32 _maxCount;
+#ifdef _WIN32
+    HANDLE hnd;
+#else
+    pthread_mutex_t _mutex;
+    pthread_cond_t  _cond;
+#endif
+} CSemaphore;
+#define Semaphore_Construct(p) (p)->_created = 0
 #define Semaphore_IsCreated(p) (*(p) != NULL)
-#define Semaphore_Close(p) HandlePtr_Close(p)
-#define Semaphore_Wait(p) Handle_WaitObject(*(p))
+WRes Semaphore_Wait(CSemaphore *p);
+WRes Semaphore_Close(CSemaphore *p);
 WRes Semaphore_Create(CSemaphore *p, UInt32 initCount, UInt32 maxCount);
 WRes Semaphore_ReleaseN(CSemaphore *p, UInt32 num);
 WRes Semaphore_Release1(CSemaphore *p);
 
-typedef CRITICAL_SECTION CCriticalSection;
+typedef struct {
+#ifdef _WIN32
+    CRITICAL_SECTION cs;
+#else
+    pthread_mutex_t _mutex;
+#endif
+} CCriticalSection;
+
 WRes CriticalSection_Init(CCriticalSection *p);
-#define CriticalSection_Delete(p) DeleteCriticalSection(p)
-#define CriticalSection_Enter(p) EnterCriticalSection(p)
-#define CriticalSection_Leave(p) LeaveCriticalSection(p)
+void CriticalSection_Delete(CCriticalSection *p);
+void CriticalSection_Enter(CCriticalSection *p);
+void CriticalSection_Leave(CCriticalSection *p);
 
 EXTERN_C_END
 
